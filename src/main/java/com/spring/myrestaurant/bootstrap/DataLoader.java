bceb95bd8f50cad97299a22500bad72df92386f2
fenonq <fenyk.ttt@gmail.com>
2022-12-27 00:08:34 +0200

    configured H2 database + updated DataLoader

--- src/main/java/com/spring/myrestaurant/bootstrap/DataLoader.java
+++ src/main/java/com/spring/myrestaurant/bootstrap/DataLoader.java
@@ -14,8 +14,8 @@ public class DataLoader implements CommandLineRunner {
     private final UserService userService;
     private final RoleService roleService;
     private final DishService dishService;
-    private final CategoryService categoryService;
     private final StatusService statusService;
+    private final CategoryService categoryService;
 
     @Override
     public void run(String... args) {
@@ -58,17 +58,52 @@ public class DataLoader implements CommandLineRunner {
             userService.changeRole(userDto2.getId(), role_admin.getId());
 
             CategoryDto categoryDto1 = CategoryDto.builder()
-                    .name("category1")
+                    .name("First dishes")
                     .build();
 
             DishDto dishDto1 = DishDto.builder()
-                    .name("dish1")
-                    .description("description1")
+                    .name("Borsch")
+                    .description("Made from chopped beets, cabbage with potatoes and various spices")
                     .category(categoryService.save(categoryDto1))
-                    .price(123)
-                    .weight(123)
+                    .price(105)
+                    .weight(350)
                     .build();
             dishService.save(dishDto1);
+
+            CategoryDto categoryDto2 = CategoryDto.builder()
+                    .name("Second dishes")
+                    .build();
+
+            DishDto dishDto2 = DishDto.builder()
+                    .name("Varenyky")
+                    .description("Varenyky with potatoes, mushrooms and sour cream")
+                    .category(categoryService.save(categoryDto2))
+                    .price(107)
+                    .weight(250)
+                    .build();
+            dishService.save(dishDto2);
+
+            DishDto dishDto3 = DishDto.builder()
+                    .name("Potato pancakes")
+                    .description("Fried potato pancakes in sauce with cherry tomatoes and sour cream")
+                    .category(categoryService.save(categoryDto2))
+                    .price(123)
+                    .weight(250)
+                    .build();
+            dishService.save(dishDto3);
+
+            CategoryDto categoryDto3 = CategoryDto.builder()
+                    .name("Second dishes")
+                    .build();
+
+            DishDto dishDto4 = DishDto.builder()
+                    .name("Caesar salad")
+                    .description("Salad of chicken, croutons, parmesan cheese, tomatoes and sauce")
+                    .category(categoryService.save(categoryDto3))
+                    .price(150)
+                    .weight(250)
+                    .build();
+            dishService.save(dishDto4);
 
             statusService.save(StatusDto.builder()
                     .name("New")
